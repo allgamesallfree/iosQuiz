9ab7ff1175400068eed366a2abd9b2d5e88d69dc
Maxwell Stein <max34563@gmail.com>
2014-11-21 11:43:36 -0500

    Initial Commit

---
+++ iosQuiz/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  iosQuiz
+//
+//  Created by Maxwell Stein on 11/21/14.
+//  Copyright (c) 2014 Maxwell Stein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
---
+++ iosQuiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iosQuiz
+//
+//  Created by Maxwell Stein on 11/21/14.
+//  Copyright (c) 2014 Maxwell Stein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
